b719dbb7c2ee4b32a0c5c5e7036135d4fe060cd5
Basma Essam <Basma.Essam@STA.com>
2019-07-17 16:56:43 +0200

    Task4

--- Task1/app/directives.js
+++ Task1/app/directives.js
@@ -1,2 +1,83 @@
 'use strict';
+/*eventsApp.directive('mySample',function(){
+    return{
+        link:function(scope,element,attrs,controller){
+            var markup="<input type='text' ng-model='sampleData'/>{{sampleData}}<br/>"
+            angular.element(element).html(markup);
+        }
+    }
+});
+*/
 
+
+eventsApp.directive('eventThumbnail',function(){
+    return{
+        restrict:'E',
+        replace: true,
+        templateUrl:'/templates/directives/eventThumbnail.html',
+        scope:{
+            event: "=event"
+        }
+    }
+});
+
+eventsApp.directive('sessionThumbnail',function(){
+    return{
+        restrict:'E',
+        replace: true,
+        templateUrl:'/templates/directives/sessionThumbnail.html',
+    }
+});
+eventsApp.directive('eventDetails',function(){
+    return{
+        restrict:'E',
+        replace: true,
+        templateUrl:'/templates/directives/eventDetails.html',
+    }
+});
+eventsApp.directive('upVote',function(){
+    return{
+        restrict:'E',
+        replace: true,
+        templateUrl:'/templates/directives/upVote.html',
+        scope:{
+            upvote:"&",
+            downvote:"&",
+            count:"="
+        }
+    }
+});
+eventsApp.directive('focusInput', function($timeout) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs, controller) {
+            $timeout(function(){
+                element[0].focus();
+            }, 0);
+        }
+    };
+});
+
+eventsApp.directive('pressEnter', function() {
+    return {
+        restrict:'A',
+        link: function(scope, element, attrs, controller) {
+            element.bind("keydown keypress", function(event) {
+                if (event.keyCode===13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.ngClick);
+                    });   
+                    
+                }
+            })
+        }
+    };
+});
+
+eventsApp.directive('dateInput',function(){
+    return{
+        restrict:'E',
+        replace: true,
+        templateUrl:'/templates/directives/dateInput.html',
+    }
+});
